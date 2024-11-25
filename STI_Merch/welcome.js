e8b6c705a89538d2cd30488e91cd036e2f026a93
CTGre15 <gregoriocharlie15@gmail.com>
2024-11-26 05:43:18 +0800

    Delete STI_Merch/welcome.js
    Changed to functions.js

--- STI_Merch/welcome.js
+++
@@ -1,9 +0,0 @@
-function openRegister(){
-    document.getElementById("loginform").style.display = "none";
-    document.getElementById("registerform").style.display = "block";
-}
-
-function openLogin(){
-    document.getElementById("registerform").style.display = "none";
-    document.getElementById("loginform").style.display = "block";
-}
