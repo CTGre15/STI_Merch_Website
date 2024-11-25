f6c03bf660fab13c49e1e2da1b4e9a7a5ebd039c
CTGre15 <gregoriocharlie15@gmail.com>
2024-11-26 07:02:02 +0800

    Add item listings

--- STI_Merch/adminPage.php
+++ STI_Merch/adminPage.php
@@ -6,26 +6,50 @@
         <script src="functions.js"></script>
     </head>
     <body>
-        Welcome Admin <br>
+        <h1>Welcome Admin</h1><br>
+        
+        <form method="post">
+            <h3>List an Item</h3><br>
+            <label for="itemName">Item Name:</label>
+            <input type="text" id="itemName" name="itemName" required><br>
+            <label for="itemDesc">Item Description:</label>
+            <input type="text" id="itemDesc" name="itemDesc" required><br>
+            <label for="price">Price:</label>
+            <input type="int" id="price" name="price" required><br>
+            <label for="stocks">Stocks:</label>
+            <input type="int" id="stocks" name="stocks" required><br>
+            Attach Image: -Ginagawa pa- <br>
+            <button name="listItem">List Item</button>
+        </form>
         <form method="post">
             <button name="logout">Log out</button>
         </form>
-        <form method="post">
-            <button name="listItem">List an Item</button>
-        </form>
         <?php
+            function listItem($itemName, $itemDesc, $price, $stocks){
+                $db = $_SESSION['db'];
+                $insertItemQuery = $db->prepare("INSERT INTO Items (itemName, itemDesc, price, stocks)
+                VALUES (?, ?, ?, ?)");
+                $insertItemQuery->bind_param("ssii", $itemName, $itemDesc, $price, $stocks);
+                $insertItemQuery->execute();
+                alert("Item Listed");
+            }
+            function passItemVariables(){
+                $itemName = $_POST["itemName"];
+                $itemDesc = $_POST["itemDesc"];
+                $price = $_POST["price"];
+                $stocks = $_POST["stocks"];
+
+                listItem($itemName, $itemDesc, $price, $stocks);
+            }
             function logout(){
                 session_destroy();
                 header("Location: welcome.php");
-            }
-            function listItem(){
-                
             }
             if(isset($_POST["logout"])) {
                 logout();
             }
             if(isset($_POST["listItem"])) {
-                listItem();
+                passItemVariables();
             }
         ?>
     </body>
